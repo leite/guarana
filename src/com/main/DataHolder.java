5d40a0d4cb4f23c5e4cba663f0c4e7271375706f
leite <xxleite@gmail.com>
2012-12-11 01:43:27 -0200

    new data model and index

---
+++ src/com/main/DataHolder.java
@@ -0,0 +1,10 @@
+package com.main;
+
+import java.util.List;
+
+public class DataHolder {
+	public List<Cache> caches;
+	public List<Host> hosts;
+	public String[] cachesIndex = new String[] {};
+	public Long[] hostsIndex    = new Long[] {};
+}
